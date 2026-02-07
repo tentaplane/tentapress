394b91003700736e1007ae141bc69d5f0c67e9ea
Chris Southam <chris@cjsoutham.com>
2026-02-07 07:52:29 +0000

    fix(menus): match block editor action icons

--- plugins/tentapress/menus/README.md
+++ plugins/tentapress/menus/README.md
@@ -7,7 +7,7 @@ Navigation menu management for TentaPress.
 | Field | Value |
 |-------|-------|
 | ID | `tentapress/menus` |
-| Version | 0.2.3 |
+| Version | 0.2.4 |
 | Provider | `TentaPress\Menus\MenusServiceProvider` |
 
 ## Goal
--- plugins/tentapress/menus/resources/views/menus/edit.blade.php
+++ plugins/tentapress/menus/resources/views/menus/edit.blade.php
@@ -230,11 +230,8 @@
                                                     title="Move up"
                                                     aria-label="Move up"
                                                     @click="move(index, -1)">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
-                                                        <path
-                                                            fill-rule="evenodd"
-                                                            d="M9.47 4.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1-1.06 1.06L10.75 6.56V15a.75.75 0 0 1-1.5 0V6.56L6.28 9.53a.75.75 0 0 1-1.06-1.06l4.25-4.25Z"
-                                                            clip-rule="evenodd" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
                                                     </svg>
                                                 </button>
                                                 <button
@@ -244,11 +241,8 @@
                                                     title="Move down"
                                                     aria-label="Move down"
                                                     @click="move(index, 1)">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
-                                                        <path
-                                                            fill-rule="evenodd"
-                                                            d="M10.53 15.78a.75.75 0 0 1-1.06 0l-4.25-4.25a.75.75 0 1 1 1.06-1.06l2.97 2.97V5a.75.75 0 0 1 1.5 0v8.44l2.97-2.97a.75.75 0 1 1 1.06 1.06l-4.25 4.25Z"
-                                                            clip-rule="evenodd" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
                                                     </svg>
                                                 </button>
                                                 <button
@@ -257,11 +251,8 @@
                                                     title="Remove item"
                                                     aria-label="Remove item"
                                                     @click="remove(index)">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
-                                                        <path
-                                                            fill-rule="evenodd"
-                                                            d="M8.75 3.5a1.75 1.75 0 0 0-1.738 1.545L6.92 6H4.75a.75.75 0 0 0 0 1.5h.314l.69 8.278A2.75 2.75 0 0 0 8.494 18.5h3.012a2.75 2.75 0 0 0 2.74-2.722l.69-8.278h.314a.75.75 0 0 0 0-1.5h-2.17l-.092-.955A1.75 1.75 0 0 0 11.25 3.5h-2.5Zm2.732 2.5-.092-.955a.25.25 0 0 0-.248-.22h-2.284a.25.25 0 0 0-.248.22L8.518 6h2.964Z"
-                                                            clip-rule="evenodd" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                     </svg>
                                                 </button>
                                             </div>
